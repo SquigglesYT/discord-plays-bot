84108a8e651feedcacc3c88497ba910f4c789a9d
Squiggles <amongus201813@gmail.com>
2026-05-29 22:11:07 -0300

    Removed commands list, made message lowercase before processing, and fixed other stuff

--- discord plays.py
+++ discord plays.py
@@ -5,7 +5,6 @@ import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
-commands = ['w', 'a', 's', 'd', 'lA', 'lW', 'lS', 'lD', 'qW', 'qA', 'qS', 'qD', 'z', 'x', 'c',]
 
 client = discord.Client(intents=intents)
 
@@ -17,55 +16,54 @@ async def on_message(message):
     user_message = message.content
 
     print(f"{user_message}")
-    if not user_message.startswith(tuple(commands)):
-        await message.channel.send("Command Invalid.")  
-    elif user_message.startswith('w')
-        pydirectinput.keyDown('up')
-        await asyncio.sleep(0.3)
-        pydirectinput.keyUp('up')
-    elif user_message.startswith('a')
-        pydirectinput.keyDown('left')
-        await asyncio.sleep(0.3)
-        pydirectinput.keyUp('left')
-    elif user_message.startswith('s')
-        pydirectinput.keyDown('down')
-        await asyncio.sleep(0.3)
-        pydirectinput.keyUp('down')
-    elif user_message.startswith('d')
-        pydirectinput.keyDown('right')
-        await asyncio.sleep(0.3)
-        pydirectinput.keyUp('right')
-    elif user_message.startswith('lW'):
+    user_message = user_message.lower()
+    if user_message.startswith('lw'):
         pydirectinput.keyDown('up')
         await asyncio.sleep(2.5)
         pydirectinput.keyUp('up')
-    elif user_message.startswith('lA'):
+    elif user_message.startswith('la'):
         pydirectinput.keyDown('left')
         await asyncio.sleep(2.5)
         pydirectinput.keyUp('left')
-    elif user_message.startswith('lS'):
+    elif user_message.startswith('ls'):
         pydirectinput.keyDown('down')
         await asyncio.sleep(2.5)
         pydirectinput.keyUp('down')
-    elif user_message.startswith('lD'):
+    elif user_message.startswith('ld'):
         pydirectinput.keyDown('right')
         await asyncio.sleep(2.5)
         pydirectinput.keyUp('right')
-    elif user_message.startswith('qW'):
+    elif user_message.startswith('qw'):
         pydirectinput.keyDown('up')
         await asyncio.sleep(0.1)
         pydirectinput.keyUp('up')
-    elif user_message.startswith('qA'):
+    elif user_message.startswith('qa'):
         pydirectinput.keyDown('left')
         await asyncio.sleep(0.1)
         pydirectinput.keyUp('left')
-    elif user_message.startswith('qS'):
+    elif user_message.startswith('qs'):
         pydirectinput.keyDown('down')
         await asyncio.sleep(0.1)
         pydirectinput.keyUp('down')
-    elif user_message.startswith('qD'):
+    elif user_message.startswith('qd'):
         pydirectinput.keyDown('right')
         await asyncio.sleep(0.1)
+        pydirectinput.keyUp('right')
+    elif user_message.startswith('w'):
+        pydirectinput.keyDown('up')
+        await asyncio.sleep(0.3)
+        pydirectinput.keyUp('up')
+    elif user_message.startswith('a'):
+        pydirectinput.keyDown('left')
+        await asyncio.sleep(0.3)
+        pydirectinput.keyUp('left')
+    elif user_message.startswith('s'):
+        pydirectinput.keyDown('down')
+        await asyncio.sleep(0.3)
+        pydirectinput.keyUp('down')
+    elif user_message.startswith('d'):
+        pydirectinput.keyDown('right')
+        await asyncio.sleep(0.3)
         pydirectinput.keyUp('right')
     elif user_message.startswith('z'):
         pydirectinput.press('z', presses=1, interval=0.0)
@@ -73,5 +71,7 @@ async def on_message(message):
         pydirectinput.press('x', presses=1, interval=0.0)
     elif user_message.startswith('c'):
         pydirectinput.press('c', presses=1, interval=0.0)
+    else:
+        pass
 
 client.run("PUT YOUR BOTS TOKEN HERE (still in these quotes)")
